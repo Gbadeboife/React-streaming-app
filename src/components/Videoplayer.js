b73524faf042f4ee99d13d4a4fae8a07d4463820
Gbadeboife <85800094+Gbadeboife@users.noreply.github.com>
2022-10-13 20:19:53 +0100

    m

---
+++ src/components/Videoplayer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Player,
+  DefaultUi,
+  Video
+} from "@vime/react";
+
+ function VideoPlayer() {
+
+  return (
+    <div className="video">
+    <Player>
+      <Video crossOrigin=""  poster="https://media.vimejs.com/poster.png">
+        <source data-src="https://media.vimejs.com/720p.mp4" type="video/mp4" />
+        <track
+          default
+          kind="subtitles"
+          src="https://media.vimejs.com/subs/english.vtt"
+          srcLang="en"
+          label="English"
+        />
+      </Video>
+      <DefaultUi ></DefaultUi>
+    </Player>
+    </div>
+  );
+}
+
+
+export default VideoPlayer
